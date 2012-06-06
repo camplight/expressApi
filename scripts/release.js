57a5fc3fb7bf0f5a5ce448cd149540c7d50dbd4c
Boris Filipov <obiwonn@gmail.com>
2012-06-06 15:25:58 +0300

    improvements to release script

--- scripts/release.js
+++ scripts/release.js
@@ -9,5 +9,6 @@ cd(__dirname+"/../");
 exec("git flow release start "+newVersion);
 sed('-i', 'version: '+p.version, 'version: '+newVersion, "package.json");
 exec("git commit -am '"+newVersion+" release'");
-//exec("git flow release finish "+newVersion);
-//exec("npm publish");
+exec("git flow release publish "+newVersion);
+exec("npm publish");
+exec("git flow release finish -F -p "+newVersion);
