4abfc092f828b500769c4fe3d70033a57b262fe2
Boris Filipov <obiwonn@gmail.com>
2012-06-06 15:12:05 +0300

    improvements on release script

--- scripts/release.js
+++ scripts/release.js
@@ -6,8 +6,8 @@ newVersion[2] = (parseInt(newVersion[2])+1).toString();
 newVersion = newVersion.join(".");
 
 cd(__dirname+"/../");
-exec("git flow release start "+newVersion);
-sed('-i', 'version: '+p.version, 'version: '+newVersion, "package.json");
-exec("git commit -am '"+newVersion+" release'");
-exec("git flow release finish");
-exec("npm publish");
+if(exec("git flow release start "+newVersion).code != 0) return;
+if(sed('-i', 'version: '+p.version, 'version: '+newVersion, "package.json").code != 0) return;
+if(exec("git commit -am '"+newVersion+" release'").code != 0) return;
+if(exec("git flow release finish").code != 0) return;
+if(exec("npm publish").code != 0) return;
