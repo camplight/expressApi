23b73f60f20ea090da2745a980301939eef42374
Boris Filipov <obiwonn@gmail.com>
2012-06-16 13:23:15 +0300

    set all headers allowed in access-control-allow-headers

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.useExpressApiMiddleware = function(options){
   app.use(function(req, res, next){
     req.store = store;
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "*");
     next();
   });
   app.use(express.methodOverride());
